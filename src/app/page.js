33b9b8fcc621b156db8975bc55f96e5f06617369
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-09-19 17:11:29 +0500

    learning

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,26 @@
+'use client'
 import Image from "next/image";
+import Link from "next/link";
 
+// Use useRouter from next/navigation
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter(); // Correct useRouter from 'next/navigation'
+  function handleNavigate() {
+    router.push('/products')
+  }
+
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <main className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Welcome to next.js Course</h1>
-    </div>
+      <Link href={"/products"}>Navigate to Products page</Link>
+      <Link href={"/account"}>Navigate to Account page</Link>
+
+      <h2 className="font-bold mt-3 text-lg">Alternative Way of Navigating using useRouter</h2>
+      <button onClick={handleNavigate}>Navigate to Products page using useRouter</button>
+    </main>
   );
 }
---
+++ src/app/products/[...product-review]/page.js
@@ -0,0 +1,5 @@
+export default function ProductReview() {
+    return (<div>
+        <h1>This is a product review page and this is a catch all segment / catch</h1>
+    </div>);
+}
